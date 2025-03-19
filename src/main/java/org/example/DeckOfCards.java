5fbea7cf7883c0ab37addf4e98533c2a2f31ee36
OskarHoddøLindh <oskarhl@stud.ntnu.no>
2025-03-19 17:10:52 +0100

    metode dealHand(int n)

--- src/main/java/org/example/DeckOfCards.java
+++ src/main/java/org/example/DeckOfCards.java
@@ -2,8 +2,20 @@ package org.example;
 import java.util.Random;
 
 public class DeckOfCards {
-    private final char[] suit = {'S', 'H', 'D', 'C'};
+    private final char[] suits = {'S', 'H', 'D', 'C'};
     private final String[] value = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13"};
+
+    public String[] dealHand(int n) {
+        String[] hand = new String[n];
+        Random random = new Random();
+
+        for (int i = 0; i<n; i++) {
+            char suit = suits[random.nextInt(suits.length)];
+            String rank = value[random.nextInt(value.length)];
+            hand[i] = suit + rank;
+        }
+        return hand;
+    }
 
 
 }
