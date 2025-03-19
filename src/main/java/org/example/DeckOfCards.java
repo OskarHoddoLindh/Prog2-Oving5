c0c963966fb5edfcde6a4266c3da62eba4239553
OskarHoddøLindh <oskarhl@stud.ntnu.no>
2025-03-19 16:53:03 +0100

    created class DeckOfCards + char variable + value variable

---
+++ src/main/java/org/example/DeckOfCards.java
@@ -0,0 +1,12 @@
+package org.example;
+import java.util.Random;
+
+public class DeckOfCards {
+    private final char[] suit = {'S', 'H', 'D', 'C'};
+    private final String[] value = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13"};
+
+
+}
+
+
+
